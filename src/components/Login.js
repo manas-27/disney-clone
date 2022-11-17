dd722357bdb518ad047a32004425cc5cd75e7fc2
Achyutmanas Tiwari <ammt501@gmail.com>
2022-11-17 23:01:04 +0530

    background image added

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,12 @@ import React from "react";
 const Login = (props) => {
   return (
     <Container>
-      <Content>Login</Content>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" />
+        </CTA>
+        <BgImage />
+      </Content>
     </Container>
   );
 };
@@ -13,7 +18,53 @@ const Container = styled.section`
   overflow: hidden;
   display: flex;
   flex-direction: column;
+  text-align: center;
+  height: 100vh;
 `;
-const Content = styled.div``;
+const Content = styled.div`
+  //   margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
 
 export default Login;
